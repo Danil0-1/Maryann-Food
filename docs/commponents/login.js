aca072c4ba391a82798123493d7c783d3d3542d4
Danil0-1 <danilomuskus1@gmail.com>
2025-05-06 15:48:34 -0500

    feat: :tada: Creacion del inicio de informacion e inicio de usuario

---
+++ docs/commponents/login.js
@@ -0,0 +1,15 @@
+
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+  
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+  
+    if (user === "Maryann" && pass === "250125") {
+      localStorage.setItem("loggedIn", "true");
+      window.location.href = "informacion.html";
+    } else {
+      alert("Usuario o contraseña incorrectos.");
+    }
+  });
+  
